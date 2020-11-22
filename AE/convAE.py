8744e5d51733d6dd6d6ecdfc3cb38b4ee576529b
hjraad <h.jamali.rad@gmail.com>
2020-11-22 19:47:08 +0100

    Starting to include evaluation

--- AE/convAE.py
+++ AE/convAE.py
@@ -12,17 +12,28 @@ from torch.utils.data.sampler import SubsetRandomSampler
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
-import sklearn
-
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+# ----------------------------------
+# Initialization
+# ----------------------------------
+convAE = 2 # 1 or 2
+TRAIN_FLAG = False
+
+data_root_dir = '../data'
+model_path_root = "./model_files/"
+results_root_dir = '../results/AE'
+
+# ----------------------------------
+# Prepare data 
+# ----------------------------------
 # convert data to torch.FloatTensor
 transform = transforms.ToTensor()
 
 # load the training and test datasets
-train_data = datasets.MNIST(root='../data', train=True,
+train_data = datasets.MNIST(root=data_root_dir, train=True,
                                    download=True, transform=transform)
-test_data = datasets.MNIST(root='../data', train=False,
+test_data = datasets.MNIST(root=data_root_dir, train=False,
                                   download=True, transform=transform)
 
 # Create training and test dataloaders
@@ -30,8 +41,6 @@ test_data = datasets.MNIST(root='../data', train=False,
 num_workers = 0
 # how many samples per batch to load
 batch_size = 20
-
-latent_size = 2
 
 # prepare data loaders
 train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, num_workers=num_workers)
@@ -48,6 +57,7 @@ import matplotlib.pyplot as plt
 #     img = img / 2 + 0.5  # unnormalize
 #     plt.imshow(np.transpose(img, (1, 2, 0)))  # convert from Tensor image
     
+
 def imshow(img):
     img = np.squeeze(img, axis=0) 
     plt.imshow(img)  # convert from Tensor image
@@ -68,63 +78,111 @@ for idx in np.arange(20):
     ax = fig.add_subplot(2, 20/2, idx+1, xticks=[], yticks=[])
     imshow(images[idx])
     ax.set_title(classes[labels[idx]])
-# plt.show()
+plt.savefig(f'{results_root_dir}/train_data_samples.jpg')
             
 # ----------------------------------
-# Build the autoencoder (AE)
+# Build a convolutional autoencoder (AE)
 # ----------------------------------
 import torch.nn as nn
 import torch.nn.functional as F
 
-# define the NN architecture
-class ConvAutoencoder(nn.Module):
-    def __init__(self):
-        super(ConvAutoencoder, self).__init__()
-        ## encoder layers ##
-        # conv layer (depth from 1 --> 16), 3x3 kernels
-        self.conv1 = nn.Conv2d(1, 16, 3, padding=1)  
-        # conv layer (depth from 16 --> 4), 3x3 kernels
-        self.conv2 = nn.Conv2d(16, 4, 3, padding=1)
-        # pooling layer to reduce x-y dims by two; kernel and stride of 2
-        self.pool = nn.MaxPool2d(2, 2)
-        # dense layers
-        self.fc1 = nn.Linear(7*7*4, 64) #flattening (input should be calculated by a forward pass - stupidity of Pytorch)
-        self.fc2 = nn.Linear(64, 2) # 2 Dim visualization
-        
-        ## decoder layers ##
-        # decoding dense layer
-        self.dec_linear_2 = nn.Linear(2, 64)
-        self.dec_linear_1 = nn.Linear(64, 7*7*4)
-        ## a kernel of 2 and a stride of 2 will increase the spatial dims by 2
-        self.t_conv1 = nn.ConvTranspose2d(4, 16, 2, stride=2)
-        self.t_conv2 = nn.ConvTranspose2d(16, 1, 2, stride=2)
+if convAE == 1:
+    latent_size = 2
+    # define the NN architecture
+    class ConvAutoencoder(nn.Module):
+        def __init__(self):
+            super(ConvAutoencoder, self).__init__()
+            ## encoder layers ##
+            # conv layer (depth from 1 --> 16), 3x3 kernels
+            self.conv1 = nn.Conv2d(1, 16, 3, padding=1)  
+            # conv layer (depth from 16 --> 4), 3x3 kernels
+            self.conv2 = nn.Conv2d(16, 4, 3, padding=1)
+            # pooling layer to reduce x-y dims by two; kernel and stride of 2
+            self.pool = nn.MaxPool2d(2, 2)
+            # dense layers
+            self.fc1 = nn.Linear(7*7*4, 64) #flattening (input should be calculated by a forward pass - stupidity of Pytorch)
+            self.fc2 = nn.Linear(64, latent_size) # 2 Dim visualization
+            
+            ## decoder layers ##
+            # decoding dense layer
+            self.dec_linear_2 = nn.Linear(latent_size, 64)
+            self.dec_linear_1 = nn.Linear(64, 7*7*4)
+            ## a kernel of 2 and a stride of 2 will increase the spatial dims by 2
+            self.t_conv1 = nn.ConvTranspose2d(4, 16, 2, stride=2)
+            self.t_conv2 = nn.ConvTranspose2d(16, 1, 2, stride=2)
 
-    def forward(self, x, return_comp=True):
-        ## ==== encode ==== ##
-        # add hidden layers with relu activation function
-        # and maxpooling after
-        x = F.relu(self.conv1(x))
-        x = self.pool(x)
-        # add second hidden layer
-        x = F.relu(self.conv2(x))
-        x = self.pool(x)  
-        # flatten and apply dense layer
-        x = x.view(-1, 7*7*4)
-        x = self.fc1(x) # compressed layer
-        x_comp = self.fc2(x) # compressed layer
-        
-        ## ==== decode ==== ##
-        x = self.dec_linear_2(x_comp)
-        x = self.dec_linear_1(x)
-        # add transpose conv layers, with relu activation function
-        x = F.relu(self.t_conv1(x.view(-1, 4, 7, 7)))
-        # output layer (with sigmoid for scaling from 0 to 1)
-        x = F.sigmoid(self.t_conv2(x))
-                
-        if return_comp:
-            return x, x_comp
-        else:
-            return x
+        def forward(self, x, return_comp=True):
+            ## ==== encode ==== ##
+            # add hidden layers with relu activation function
+            # and maxpooling after
+            x = F.relu(self.conv1(x))
+            x = self.pool(x)
+            # add second hidden layer
+            x = F.relu(self.conv2(x))
+            x = self.pool(x)  
+            # flatten and apply dense layer
+            x = x.view(-1, 7*7*4)
+            x = self.fc1(x) # compressed layer
+            x_comp = self.fc2(x) # compressed layer
+            
+            ## ==== decode ==== ##
+            x = self.dec_linear_2(x_comp)
+            x = self.dec_linear_1(x)
+            # add transpose conv layers, with relu activation function
+            x = F.relu(self.t_conv1(x.view(-1, 4, 7, 7)))
+            # output layer (with sigmoid for scaling from 0 to 1)
+            x = F.sigmoid(self.t_conv2(x))
+                    
+            if return_comp:
+                return x, x_comp
+            else:
+                return x
+if convAE == 2:    
+    latent_size = 128  
+    class ConvAutoencoder(nn.Module):
+        def __init__(self):
+            super(ConvAutoencoder, self).__init__()
+            ## encoder layers ##
+            # conv layer (depth from 1 --> 16), 3x3 kernels
+            self.conv1 = nn.Conv2d(1, 16, 3, padding=1)  
+            # conv layer (depth from 16 --> 4), 3x3 kernels
+            self.conv2 = nn.Conv2d(16, 4, 3, padding=1)
+            # pooling layer to reduce x-y dims by two; kernel and stride of 2
+            self.pool = nn.MaxPool2d(2, 2)
+            # dense layers
+            self.fc1 = nn.Linear(7*7*4, latent_size) #flattening (input should be calculated by a forward pass - stupidity of Pytorch)
+            
+            ## decoder layers ##
+            # decoding dense layer
+            self.dec_linear_1 = nn.Linear(latent_size, 7*7*4)
+            ## a kernel of 2 and a stride of 2 will increase the spatial dims by 2
+            self.t_conv1 = nn.ConvTranspose2d(4, 16, 2, stride=2)
+            self.t_conv2 = nn.ConvTranspose2d(16, 1, 2, stride=2)
+
+        def forward(self, x, return_comp=True):
+            ## ==== encode ==== ##
+            # add hidden layers with relu activation function
+            # and maxpooling after
+            x = F.relu(self.conv1(x))
+            x = self.pool(x)
+            # add second hidden layer
+            x = F.relu(self.conv2(x))
+            x = self.pool(x)  
+            # flatten and apply dense layer
+            x = x.view(-1, 7*7*4)
+            x_comp = self.fc1(x) # compressed layer
+            
+            ## ==== decode ==== ##
+            x = self.dec_linear_1(x_comp)
+            # add transpose conv layers, with relu activation function
+            x = F.relu(self.t_conv1(x.view(-1, 4, 7, 7)))
+            # output layer (with sigmoid for scaling from 0 to 1)
+            x = F.sigmoid(self.t_conv2(x))
+                    
+            if return_comp:
+                return x, x_comp
+            else:
+                return x
 
 # initialize the NN
 model = ConvAutoencoder()
@@ -133,9 +191,6 @@ print(model)
 # ----------------------------------
 # Train the AE
 # ----------------------------------
-TRAIN_FLAG = False
-model_path_root = "./model_files/"
-
 import os
 if not os.path.exists(model_path_root):
     os.makedirs(model_path_root)
@@ -190,7 +245,7 @@ if TRAIN_FLAG:
 
 # Model class must be defined somewhere
 if not TRAIN_FLAG:
-    MODEL_NAME = "model-1606041368-epoch20-latent2"
+    MODEL_NAME = "model-1606000037-epoch20-latent128"
 checkpoint = torch.load(model_path_root + MODEL_NAME)
 model.load_state_dict(checkpoint['model_state_dict'])
 optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
@@ -220,6 +275,7 @@ for idx in np.arange(20):
     ax = fig.add_subplot(2, 20/2, idx+1, xticks=[], yticks=[])
     imshow(output[idx])
     ax.set_title(classes[labels[idx]])
+plt.savefig(f'{results_root_dir}/reconstructed_test_samples_{MODEL_NAME}.jpg')
 
 # ----------------------------------
 # Visualize the latent vector
@@ -237,11 +293,20 @@ _, embeddings = model(full_test_data_tensor)
 embeddings_np = embeddings.detach().numpy()
 full_test_labels_tensor_np = full_test_labels_tensor.numpy()
 
-plt.figure()
-classes = [str(nr) for nr in range(0,10)]
-plt.scatter(embeddings_np[:,0], embeddings_np[:,1], c=full_test_labels_tensor_np, 
-            s=8, cmap='tab10', label=classes)
-plt.legend()
-plt.show() 
+if latent_size == 2:
+    plt.figure()
+    classes = [str(nr) for nr in range(0,10)]
+    plt.scatter(embeddings_np[:,0], embeddings_np[:,1], c=full_test_labels_tensor_np, 
+                s=8, cmap='tab10', label=classes)
+    plt.legend()
+    plt.savefig(f'{results_root_dir}/scatter_{MODEL_NAME}.jpg')
 
-
+from sklearn.manifold import TSNE
+if latent_size != 2:
+    X = embeddings_np
+    X_embedded = TSNE(n_components=2).fit_transform(X)
+    plt.figure()
+    plt.scatter(X_embedded[:,0], X_embedded[:,1], c=full_test_labels_tensor_np, 
+                s=8, cmap='tab10', label=classes)
+    plt.legend()
+    plt.savefig(f'{results_root_dir}/scatter_{MODEL_NAME}.jpg')
