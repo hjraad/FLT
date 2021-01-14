4cd42b53bdfc50f14345754114eccb1b9314a395
hjraad <h.jamali.rad@gmail.com>
2021-01-14 12:24:02 +0100

    some saving func

--- base_fed_learning/visualize_results.py
+++ base_fed_learning/visualize_results.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
      # ----------------------------------
     plt.close('all')
 
-    results_path = f'{args.results_root_dir}/main_fed/'
+    results_path = f'{args.results_root_dir}/main_fed/scenario_4/'
     entries = os.listdir(results_path)
     
     # plot loss curve
@@ -56,4 +56,5 @@ if __name__ == '__main__':
     legend = ax.legend(loc='lower right')#, shadow=True, fontsize='x-large')    
     plt.ylabel('accuracy')
     plt.xlabel('epoch')
+    plt.savefig(f'{args.results_root_dir}/main_fed/scenario_4/result.png')
     plt.show()
