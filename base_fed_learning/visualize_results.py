b5135629e9581bc5d7d41d677b27d1ea1baff897
Mohammad <moh.abdizadeh@gmail.com>
2021-01-16 14:21:20 -0500

    added visualization changes

--- base_fed_learning/visualize_results.py
+++ base_fed_learning/visualize_results.py
@@ -12,49 +12,99 @@ import argparse
 from torchvision import datasets, transforms
 import torch
 import torchvision
-
+import numpy as np
 import pandas as pd
+from glob import glob
 
 if __name__ == '__main__':
     # parse args
     args = args_parser()
     args.device = torch.device('cuda:{}'.format(args.gpu) if torch.cuda.is_available() and args.gpu != -1 else 'cpu')
-     # ----------------------------------
+    # ----------------------------------
     plt.close('all')
 
-    results_path = f'{args.results_root_dir}/main_fed/scenario_4/'
-    entries = os.listdir(results_path)
+    # ----------------------------------
+    # plotting settings
+    plot_linewidth = 3
+    text_size = 5
+    marker_step = 5
+    marker_size = 5
+    legend_text_size = 'x-large'
+    legend_linewidth = 4
+
+    # -----------------------------------
+
+    result_directory_name = f'{args.results_root_dir}/main_fed/'
+    folder_list = sorted( glob(f'{result_directory_name}/scenario_*/') )
+    print(folder_list)
+    for folder in folder_list:
+        results_path = folder
+        entries = sorted( glob(f'{results_path}/Scenario*.csv') )
     
-    # plot loss curve
-    fig, ax = plt.subplots()
-    line_style = ['k--', 'k:', 'r--', 'r:', 'b--', 'b:', 'g--', 'g:']
-    counter = 0
-    for (idx, entry) in enumerate(entries):
-        print(idx)
-        if not entry.endswith(".csv"):
-            continue
-
-        method = ''
-        print(idx, entry)
-        index0 = entry.find('method')
-        index1 = entry.find('multicenter')
-
-        if entry[index1+12] != '0':
-            method = 'multicenter'
-        else:
-            method = entry[index0+7:index1-1]
+        # plot loss curve
+        fig, ax = plt.subplots()
+        line_style = ['k^-', 'k-.', 'rs-', 'r-.', 'bo-', 'b-.', 'gd-', 'g-.']
         
-        print(f'clustering method is {method}')
+        counter = 0
+        for (idx, entry) in enumerate(entries):
+            print(idx)
 
-        df = pd.read_csv(f'{results_path}/{entry}')
+            filename_decoded = entry.split("/")[-1].split("_")
+            if len(filename_decoded) < 3:
+                print("Error in filename")
+                continue
 
-        ax.plot(range(len(df['training_accuracy'])), df['training_accuracy'], line_style[2*counter], label=f'{method}: train_accuracy')
-        ax.plot(range(len(df['test_accuracy'])), df['test_accuracy'], line_style[2*counter+1], label=f'{method}: test_accuracy')
+            dataset = filename_decoded[1]
+            clustering_method = filename_decoded[2].split(".")[0]
+       
+            print(f'clustering method is {clustering_method}')
 
-        counter += 1
+            try:
+                df = pd.read_csv(f'{entry}')
+            except:
+                print("Error reading file")
+                continue
 
-    legend = ax.legend(loc='lower right')#, shadow=True, fontsize='x-large')    
-    plt.ylabel('accuracy')
-    plt.xlabel('epoch')
-    Plt.savefig()
-    plt.show(f'{args.results_root_dir}/main_fed/scenario_3/result.png')
+            
+            markers_on = list(np.arange(0, df.shape[0], marker_step))
+
+            ax.plot(range(len(df['training_accuracy'])), df['training_accuracy'], line_style[2*counter], 
+                    label=f'{clustering_method}: train_accuracy', linewidth =plot_linewidth, markevery=markers_on, markersize = marker_size)
+            ax.plot(range(len(df['test_accuracy'])), df['test_accuracy'], line_style[2*counter+1], 
+                    label=f'{clustering_method}: test_accuracy', linewidth =plot_linewidth, markevery=markers_on, markersize = marker_size)
+
+            counter += 1
+
+        plt.rcParams.update({'font.size': text_size})
+
+        legend = ax.legend(loc='lower right')#, shadow=True, fontsize='x-large')
+        leg = plt.legend()
+        # get the individual lines inside legend and set line width
+        for line in leg.get_lines():
+            line.set_linewidth(legend_linewidth)
+        # get label texts inside legend and set font size
+        for text in leg.get_texts():
+            text.set_fontsize(legend_text_size)
+        plt.grid(color='k', linestyle=':', linewidth=1, axis='y')
+        plt.ylabel('accuracy (%)')
+        plt.xlabel('epoch')
+        plt.savefig(f'{results_path}/result.png')
+        plt.show()
+"""
+-   accuracy (%)
+-    Text size (on axis, labels and titles and legend) encode in
+-   dotted line -> dashed
+-    thickness line -> encode in the program
+-    marker (^ , s, o, d), and its size encoded
+-   spacing of markers (every 5)
+    Naming convention, and order:
+        FedAvg (train),
+        FedAvg (test)
+        Local (train),
+        Local (test),
+        FedSem (train),
+        FedSem (test),
+        Ours (train)
+        Ours (test)
+-        Axis grid -> only horizontal and every 10%
+"""
