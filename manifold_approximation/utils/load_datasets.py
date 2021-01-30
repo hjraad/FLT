b2399c8e2d440e6f1f5d1cd0721fd9713dc6a186
Mohammad <moh.abdizadeh@gmail.com>
2021-01-30 16:33:10 -0500

    added relabeling to 47 labels

--- manifold_approximation/utils/load_datasets.py
+++ manifold_approximation/utils/load_datasets.py
@@ -117,7 +117,7 @@ class FEMNIST(VisionDataset):
         class_names_map = [ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9,
             10,  11,  12,  13,  14,  15,  16,  17,  18,  19,  20,  21,  22,  23,  24,  25,  26,  27,  28,  29,  30,  31,  32,  33,  34,  35, 
             36,  37,  12,  38,  39,  40,  41,  42,  18,  19,  20,  21,  22,  43,  24,  25,  44,  45,  28,  46,  30,  31,  32,  33,  34,  35]
-
+        # TODO: automate this
         if True:# 47 classess
             for i in range(len(self.users)):
                 for j in range(len(self.data0[self.users[i]]['y'])):
