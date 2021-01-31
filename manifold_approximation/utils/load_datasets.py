4e1bf2748157625144c273c6f24b27a032e359b7
Mohammad <moh.abdizadeh@gmail.com>
2021-01-31 10:21:38 -0500

    add the change for 62 labels

--- manifold_approximation/utils/load_datasets.py
+++ manifold_approximation/utils/load_datasets.py
@@ -118,7 +118,7 @@ class FEMNIST(VisionDataset):
             10,  11,  12,  13,  14,  15,  16,  17,  18,  19,  20,  21,  22,  23,  24,  25,  26,  27,  28,  29,  30,  31,  32,  33,  34,  35, 
             36,  37,  12,  38,  39,  40,  41,  42,  18,  19,  20,  21,  22,  43,  24,  25,  44,  45,  28,  46,  30,  31,  32,  33,  34,  35]
         # TODO: automate this
-        if True:# 47 classess
+        if False:# 47 classess
             for i in range(len(self.users)):
                 for j in range(len(self.data[self.users[i]]['y'])):
                     ll = self.data[self.users[i]]['y'][j]
